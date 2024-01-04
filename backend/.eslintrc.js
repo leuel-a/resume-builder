eaf641b7e947aea50d2cf63be2be4c3b3edb5212
leuel-a <leuela1993@gmail.com>
2024-01-04 18:01:53 +0300

    add backend starter files

---
+++ backend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-plusplus': 'off',
+    'no-process-exit': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
